26d14e31c2053d255cb9fbfb8b8187eebd19450b
Niatomi <playervoker@gmail.com>
2024-05-08 04:38:15 +0400

    Removed unused import

--- PhoneClient/app/src/main/java/ru/niatomi/phoneclient/components/DevBar.kt
+++ PhoneClient/app/src/main/java/ru/niatomi/phoneclient/components/DevBar.kt
@@ -3,7 +3,6 @@ package ru.niatomi.phoneclient.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
