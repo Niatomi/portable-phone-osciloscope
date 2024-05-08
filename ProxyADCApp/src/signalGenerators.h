7b3139486969f989ff7c232fae4a7854ae6b9887
Niatomi <playervoker@gmail.com>
2024-05-08 04:37:53 +0400

    Add another squarewave and change sinwave

--- ProxyADCApp/src/signalGenerators.h
+++ ProxyADCApp/src/signalGenerators.h
@@ -2,7 +2,7 @@
 
 double sinwave(double x)
 {
-    return sin(x) * 5;
+    return sin(x * RAD_TO_DEG) * 5;
 }
 
 double coswave(double x)
@@ -13,6 +13,18 @@ double coswave(double x)
 double squarewave(double x)
 {
     if (((int)x % 5) <= 5)
+    {
+        return 5.0;
+    }
+    else
+    {
+        return 0.0;
+    }
+}
+
+double squarewave2(double x)
+{
+    if (x <= 0)
     {
         return 5.0;
     }
