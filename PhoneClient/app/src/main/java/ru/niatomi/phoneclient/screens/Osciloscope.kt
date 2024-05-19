1c2d94fc0caa03687b153b5647bbab9e79b31cf1
Niatomi <playervoker@gmail.com>
2024-05-19 23:15:59 +0400

    Wraped osc logic

--- PhoneClient/app/src/main/java/ru/niatomi/phoneclient/screens/Osciloscope.kt
+++ PhoneClient/app/src/main/java/ru/niatomi/phoneclient/screens/Osciloscope.kt
@@ -1,23 +1,23 @@
 package ru.niatomi.phoneclient.screens
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
+import ru.niatomi.phoneclient.components.XYCanvas
 import ru.niatomi.phoneclient.utils.RingBuffer
 import java.time.Instant
 
@@ -25,27 +25,44 @@ import java.time.Instant
 fun Oscilloscope(
     ringBuffer: MutableState<RingBuffer<Pair<Instant, Double>>>
 ) {
-    Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(0.97f).padding(10.dp)) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black),
-            onDraw = {
-                    ringBuffer.let { buffer  ->
-                        drawRect(
-                            color = Color.Black,
-                            topLeft = Offset(x = 0.0f, y = 0.0f),
-                            size = size
-                        )
-                        for ((index, frame) in buffer.value.withIndex()) {
-                            drawRect(
-                                color = Color.White,
-                                topLeft = Offset(size.width - 40 * index, center.y + 100 * buffer.value.get(index).second.toFloat()),
-                                size = Size(10.0f, 10.0f)
-                            )
-                        }
-                    }
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(1f)
+        .padding(10.dp)) {
+
+        Column(modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxSize()
+            .padding(2.dp),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            XYCanvas(
+                x = ringBuffer.value.map { it.first.nano }.toMutableList(),
+                y = ringBuffer.value.map { it.second }.toMutableList(),
+                gridYValue = 2.0,
+                stepY = 50.0
+            )
+            Row(modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .padding(10.dp),
+                Arrangement.SpaceBetween,
+                Alignment.CenterVertically
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "+")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "-")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "*")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "/")
+                }
             }
-        )
+        }
     }
 }
